a68fb74e4aced04498542c5400a529c29316da44
Serg Tikhomirov <serge-tikhomirov@yandex.ru>
2025-01-02 14:54:20 +0300

    Пример по изучению основ синтаксиса языка Java и базовыми типами данных Ветка содержит пример ввода двух чисел и примеры основных операций с этими числами.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:  ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста : " + text.length());
+        System.out.println("Введите первое число:  ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:  ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+
+        int amount = firstNumber + secondNumber;
+        System.out.println("1) Вывод суммы        : " + firstNumber + " + " + secondNumber + " = " + amount);
+
+        int composition = firstNumber * secondNumber;
+        System.out.println("2) Вывод произведения : " + firstNumber + " * " + secondNumber + " = " + composition);
+
+        int difference = firstNumber - secondNumber;
+        System.out.println("3) Вывод разности     : " + firstNumber + " - " + secondNumber + " = " + difference);
+
+        double division = (double) firstNumber / secondNumber;
+        System.out.println("4) Вывод частного     : " + firstNumber + " / " + secondNumber + " = " + division);
+
     }
 
 }
