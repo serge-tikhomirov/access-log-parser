8ab0a0def62cebf5f974b94ca97623626e5198da
Serg Tikhomirov <serge-tikhomirov@yandex.ru>
2025-02-07 23:33:54 +0300

    Задание 1. Обработка исключений (правильный)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +12,27 @@ public class Main {
         File    file_ForCheck;           //интерфейс к атрибутам файла, для проверки
         int     int_ColFile = 0;         //количество верно указанных путей к файлам
         // конец  декларации локальных переменных метода
+/*
+●     Выполняйте задание в том же проекте AccessLogParser в ветке master.
 
+●     После проверки существования файла и того, что указанный путь является путём именно к файлу, а не к папке, напишите код, который будет построчно читать указанный файл:
+
+FileReader fileReader = new FileReader(path);
+BufferedReader reader =
+   new BufferedReader(fileReader);
+String line;
+while ((line = reader.readLine()) != null) {
+   int length = line.length();
+}
+●     Поскольку в данном коде есть целых два места, требующих обязательной обработки исключений,
+поместите этот код внутрь конструкции try…catch, внутри которой пропишите обработку всех исключений:
+
+try {
+   // code here
+} catch (Exception ex) {
+   ex.printStackTrace();
+}
+ */
 
         while (true){ //TODO бесконечный цикл без возможности штатного завершения программы (требуется заданием)
             System.out.println("Введите путь к файлу и нажмите <Enter>:  ");
@@ -31,9 +53,37 @@ public class Main {
                 continue; //TODO неявная логика (требуется заданием), правильно использовать конструкцию else if.
             }
 
+
             if (isFileExists(file_ForCheck)){
                 int_ColFile++;
                 System.out.println("Путь к файлу указан верно. Это файл номер: " + int_ColFile);
+                try{
+                    FileReader fileReader = new FileReader(text_PathOfFile);
+                    BufferedReader reader =
+                            new BufferedReader(fileReader);
+                    String text_Line;
+                    int colLinesOfFile = 0;
+                    int maxLengthOfLine = 0;
+                    int minLengthOfLine = Integer.MAX_VALUE;
+                    while ((text_Line = reader.readLine()) != null) {
+                        int length = text_Line.length();
+                        colLinesOfFile++;
+                        if(length < minLengthOfLine){
+                            minLengthOfLine = length;
+                        }
+                        if(length > maxLengthOfLine){
+                            maxLengthOfLine = length;
+                        }
+                        if(length > 1024){
+                            throw new OperationAttemptException("В файле встретилась строка длиннее 1024 символов");
+                        }
+                    }
+                    System.out.println("Общее количество строк в файле = "+colLinesOfFile);
+                    System.out.println("Длина самой длинной строки в файле = "+maxLengthOfLine);
+                    System.out.println("Длина самой короткой строки в файле = "+minLengthOfLine);
+                } catch (Exception ex){
+                    ex.printStackTrace();
+                }
             }
             else{
                 System.out.println("Статус получить невозможно. Файл еще не сформирован или не доступен для данного JAVA-процесса ");
--- src/OperationAttemptException.java
+++ src/OperationAttemptException.java
@@ -1,5 +1,3 @@
-package PACKAGE_NAME;
-
 public class OperationAttemptException extends RuntimeException {
   public OperationAttemptException(String message) {
     super(message);
